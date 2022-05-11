c666a27da0683d6f2cf703f37dd406882d8d6189
user-pc <user-pc@DESKTOP-4D7VE44>
2022-05-11 10:15:59 +0200

    Add project files.

---
+++ TodoApi/Startup.cs
@@ -0,0 +1,16 @@
+﻿using TodoApi.Models;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddLogging();
+
+   
+        }
+
+    }
+}
